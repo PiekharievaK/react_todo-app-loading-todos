ed5a3fd803d92cc267c91255e5e65d6dd32257b8
PiekharievaK <pechareva@gmail.com>
2025-03-28 05:26:34 +0200

    delete Error type file

--- src/types/Errors.ts
+++
@@ -1,7 +0,0 @@
-export type ERROR = {
-  default: '';
-  title: 'Title should not be empty';
-  add: 'Unable to add a todo';
-  delete: 'Unable to delete a todo';
-  update: 'Unable to update a todo';
-};
