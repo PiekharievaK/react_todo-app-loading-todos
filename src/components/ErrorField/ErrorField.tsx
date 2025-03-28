c7143703f07e3c1003d8534c3b31b830d4c3f9b9
PiekharievaK <pechareva@gmail.com>
2025-03-28 05:20:41 +0200

    add solution + few more options

---
+++ src/components/ErrorField/ErrorField.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type Props = {
+  errorMessage: string;
+};
+
+export const ErrorField: React.FC<Props> = ({ errorMessage }) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={`notification is-danger is-light has-text-weight-normal ErrorNotification ${!errorMessage && 'hidden'}`}
+    >
+      <button data-cy="HideErrorButton" type="button" className="delete" />
+      {errorMessage}
+    </div>
+  );
+};
